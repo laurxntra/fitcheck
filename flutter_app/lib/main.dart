748404d014c0cce934f65527e6091fe68341f918
laurxnt <laurxnt@cs.washington.edu>
2025-02-14 17:38:39 -0800

    unfinished prototype of home

--- flutter_app/lib/main.dart
+++ flutter_app/lib/main.dart
@@ -20,8 +20,94 @@ class MyApp extends StatelessWidget {
       routes: {
         '/': (context) => const PhoneLoginScreen(),
         '/otp': (context) => const OtpVerificationScreen(),
-        '/home': (context) => const HomePage(),
+        '/home': (context) => const MyHomePage(),
       },
     );
   }
 }
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key});
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  int currPage = 0;
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: HomePage(),
+      bottomNavigationBar: BottomAppBar(
+        child: Row(
+          children: [
+            Spacer(),
+            IconButton(
+              icon: Icon(
+                Icons.home,
+                color: currPage == 0 
+                ? Color.fromRGBO(255, 255, 255, 1)
+                : Color.fromRGBO(143, 143, 143, 1),
+              ), 
+              onPressed: (){
+                setState(() {
+                  currPage = 0;
+                });
+              }
+              ),
+              
+            Spacer(),
+            IconButton(
+              icon: Icon(
+                Icons.search,
+                color: currPage == 1 
+                ? Color.fromRGBO(255, 255, 255, 1)
+                : Color.fromRGBO(143, 143, 143, 1),
+              ), 
+              onPressed: (){
+                setState(() {
+                  currPage = 1;
+                });
+              }
+              ),
+
+            Spacer(),
+            IconButton(
+              icon: Icon(
+                Icons.camera,
+                color: currPage == 2 
+                ? Color.fromRGBO(255, 255, 255, 1)
+                : Color.fromRGBO(143, 143, 143, 1),
+              ), 
+              onPressed: (){
+                setState(() {
+                  currPage = 2;
+                });
+              }
+              ),
+  
+            Spacer(),
+            IconButton(
+              icon: Icon(
+                Icons.person,
+                color: currPage == 3 
+                ? Color.fromRGBO(255, 255, 255, 1)
+                : Color.fromRGBO(143, 143, 143, 1),
+              ), 
+              onPressed: (){
+                setState(() {
+                  currPage = 3;
+                });
+              }
+              ),
+            Spacer(),
+          ],
+        ),
+      ),
+    );
+  }
+
+}
+
+
--- flutter_app/lib/pages/home_page.dart
+++ flutter_app/lib/pages/home_page.dart
@@ -1,22 +1,38 @@
 import 'package:flutter/material.dart';
 
-class HomePage extends StatelessWidget {
-  const HomePage({super.key});
+class HomePage extends StatefulWidget {
+  @override
+  _HomePageState createState() => _HomePageState();
 
+}
+
+class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Colors.black,
       appBar: AppBar(
-        title: const Text('FitCheck Home'),
-        backgroundColor: Colors.blue,
-      ),
-      body: const Center(
-        child: Text(
-          'Welcome to FitCheck! 🎉',
-          style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold, color: Colors.white),
+        title: Image.asset(
+          'assets/FitCheck.png', 
+           height: 100,
         ),
+        
       ),
-    );  
+
+      body: PageView.builder(
+        scrollDirection: Axis.vertical,
+        itemCount: 10,
+        itemBuilder: (context, index) {
+          return Container(
+            color: Colors.accents[index % Colors.accents.length],
+            child: Center(
+              child: Text(
+                'Item $index',
+                style: TextStyle(fontSize: 24, color: Colors.white),
+              )
+            )
+          );
+        },
+      )
+    );
   }
 }
